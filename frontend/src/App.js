ef00cbfa2d8e5c3cd668837f59ac48836e2c4743
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-03-08 22:19:40 +0530

    FEAT: Install all dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
+
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 700,
+      easing: 'ease-in',
+      delay: 100,
+    })
+  })
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
