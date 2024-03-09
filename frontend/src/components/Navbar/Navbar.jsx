01142738fffca04f4b9b2aebe8e50da0ed43ca46
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-03-09 23:04:18 +0530

    FEAT: Navbar Complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 
 const Navbar = () => {
+  const menus = [{
+    id: 1,
+    name: "Home",
+    link:"/#"
+  },
+  {
+    id: 2,
+    name: "Services",
+    link:"/#services"
+  }]
   return (
     <div className='bg-gradient-to-r from-secondary to-secondary/90 text-white'>
       <div className="container py-2">
-        <div className="flex">
-          <div>Logo Section</div>
-          <div></div>
+        <div className="flex justify-between">
+          <div className='font-bold text-2xl sm:text-3xl flex justify-center item-center gap-2 font-serif'>Our Agency</div>
+          <div className='flex justify-between items-center gap-4'>
+            <ul className='hidden sm:flex items-end gap-6'>
+              {menus.map((menu, index) => (
+                <li key={index}>
+                  <a href={menu.link} className='inline-block text-xl py-4 gap-4'>
+                    {menu.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button className='bg-primary px-4 py-2 rounded-full hover:scale-105 duration-100'>Chat</button>
+          </div>
         </div>
       </div>
     </div>
